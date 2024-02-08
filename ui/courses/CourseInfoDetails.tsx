39a0f68bc43dc351cd605a9ed4bb565581b3dac0
shahidafrid65 <shahidafrid825@gmail.com>
2024-02-08 21:07:22 +0530

    Course Details View Added

--- ui/courses/CourseInfoDetails.tsx
+++ ui/courses/CourseInfoDetails.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import Container from '../layout/container'
-import { Card, CardContent, CardHeader } from '@/components/ui/card'
 import Title3 from '@/components/ui/text/Title3'
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
 import { Course } from '@/types/course.types'
+import CourseDetailsCard from './CourseDetailsCard'
 
 type Props = {
   course: Course
@@ -13,28 +12,8 @@ const CourseInfoDetails = ({ course }: Props) => {
   return (
     <Container className='py-8'>
       <p className='mb-8'>Time to complete: {course?.courseDuration}</p>
-      <Title3>Course Content</Title3>
-      <Card>
-        <CardContent>
-          <Accordion type="multiple" className="w-full">
-            {course?.modules?.map((module) => <AccordionItem key={module?.title} value={module?.title}>
-              <AccordionTrigger>{module?.title}</AccordionTrigger>
-              <AccordionContent>
-                <ul className='flex flex-col gap-2 pl-4'>
-                  {module?.sessions?.map((session, idx) => <li key={session?.sessionNumber}>
-                    <span>{idx + 1}. {session?.title}</span>
-
-                    <ol>
-                      {session?.topics?.map((topic) => <li key={topic}>--- {topic}</li>)}
-                    </ol>
-                  </li>)}
-                  
-                </ul>
-              </AccordionContent>
-            </AccordionItem>)}
-          </Accordion>
-        </CardContent>
-      </Card>
+      <Title3 className='px-8'>Course Content</Title3>
+      <CourseDetailsCard course={course}/>
     </Container>
   )
 }
