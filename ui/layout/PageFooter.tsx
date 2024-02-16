dec44be8dbe88b637c59dbc2a958897618e0d94e
Syed Zakeer Hussain <syed.zakeer.hussain@sap.com>
2024-02-16 15:45:34 +0530

    fix: footer alignments

--- ui/layout/PageFooter.tsx
+++ ui/layout/PageFooter.tsx
@@ -5,67 +5,72 @@ import { Button } from '@/components/ui/button'
 import Title3 from '@/components/ui/text/Title3'
 import Lead from '@/components/ui/text/Lead'
 import Link from "next/link";
+import FullstackInstituteLogo from './FullstackInstituteLogo'
+import Balancer from 'react-wrap-balancer'
 
 const About = [
-	{ name: "about us", link: "about-us" },
-	{ name: "our team", link: "our-team" }
+  { name: "about us", link: "about-us" },
+  { name: "our team", link: "our-team" }
 ]
 
 const LegalPols = [
-	{ name: "terms of usage", link: "terms-of-usage" },
-	{ name: "privacy policy", link: "privacy-policy" },
-	{ name: "cookie policy", link: "cookie-policy" },
-	{ name: "code of conduct", link: "code-of-conduct" }
+  { name: "terms of usage", link: "terms-of-usage" },
+  { name: "privacy policy", link: "privacy-policy" },
+  { name: "cookie policy", link: "cookie-policy" },
+  { name: "code of conduct", link: "code-of-conduct" }
 ]
 
 const navLinks = [
-	{ title: 'About us', links: About },
-	{ title: 'Legal policies', links: LegalPols },
+  { title: 'About us', links: About },
+  { title: 'Legal policies', links: LegalPols },
 
 ]
 
 type Props = {}
 
 const PageFooter = (props: Props) => {
-	return (
-		<div className=''>
-			<section>
-				<Container className='rounded-lg py-16'>
-					<div className='flex justify-between items-center'>
-						<div className='flex-1'>
-							<h3 className='scroll-m-20 text-2xl font-semibold tracking-tight p-8'>Fullstack</h3>
-							<Title3>Code practice and mentorship for everyone</Title3>
-							<Lead className="mt-4"> Develop fluency in 67 programming languages with our unique blend of learning, practice and mentoring. Exercism is fun, effective and 100% free, forever</Lead>
+  return (
+    <div className='mt-8 bg-slate-50'>
+      <section className='border-t border-b shadow-sm'>
+        <Container className='rounded-lg py-16'>
+          <FullstackInstituteLogo />
+          <div className='flex justify-between items-center'>
+            <div className='flex-1'>
 
-						</div>
-						<div className='flex-1 text-right'>
-							<Button size="lg" className='h-16 px-16 text-2xl'>Explore courses</Button>
-						</div>
-					</div>
+              <Balancer>
+                <Title3>Comprehensive Full Stack Coding Exercises and Expert Mentorship for All</Title3>
+              </Balancer>
+              <Lead className="mt-4">Master Full Stack Development: A Fusion of Interactive Learning, Hands-On Practice, and Personalized Mentoring. Experience the Fun and Efficacy of Fullstack Programming.</Lead>
 
-				</Container>
-			</section>
-			<section className='bg-slate-300 py-16'>
-				<Container className='flex'>
+            </div>
+            <div className='flex-1 text-right'>
+              <Button size="lg" className='h-16 px-16 text-2xl'>Explore courses</Button>
+            </div>
+          </div>
 
-					<footer className='flex gap-16'>
-						{navLinks.map(({ title, links }) => <div key={title} className="bg-neutral text-neutral-content">
-							<h6 className="scroll-m-20 text-xl font-semibold tracking-tight pb-3">{title}</h6>
-							<ul className='flex flex-col gap-2'>
-								{links.map(({ name, link }) => (
-									<li key={link}>
-										<Link href={`/${link}`}>
-											{name}
-										</Link>
-									</li>
-								))}
-							</ul>
-						</div>)}
-					</footer>
-				</Container>
-			</section>
-		</div >
-	)
+        </Container>
+      </section>
+      <section className='bg-muted py-16'>
+        <Container className='flex'>
+
+          <footer className='flex gap-16'>
+            {navLinks.map(({ title, links }) => <div key={title} className="bg-neutral text-neutral-content">
+              <h6 className="scroll-m-20 text-xl font-semibold tracking-tight pb-3">{title}</h6>
+              <ul className='flex flex-col gap-2'>
+                {links.map(({ name, link }) => (
+                  <li key={link} className='capitalize'>
+                    <Link href={`/${link}`} className='hover:text-slate-900 text-slate-600'>
+                      {name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>)}
+          </footer>
+        </Container>
+      </section>
+    </div >
+  )
 }
 
 export default PageFooter
