c7175a0bfeb15c6c2ba9a7f2fa62d4f70317ec48
Syed Zakeer Hussain <syed.zakeer.hussain@sap.com>
2024-02-20 19:21:23 +0530

    fix: lint issues

--- services/user.services.ts
+++ services/user.services.ts
@@ -1,15 +1,8 @@
 
-import crypto from 'crypto'
 import type { User as IUser } from "@prisma/client";
 import { User } from 'db/user';
-import { sendVerificationCode } from 'utils/mail.server';
-import type { IUserRegister, IUserVerification } from 'types/user.types';
-import Errors from 'contants/error.constants';
-import db from 'db';
 import HttpService from './http.services';
 import ENDPOINTS from '@/contants/api.constants';
-
-
 
 class UserService extends HttpService {
 
@@ -26,64 +19,6 @@ class UserService extends HttpService {
     } catch (error) {
       console.log(":: signIn ERROR ::", error)
     }
-  }
-
-  // public static async registerAndGenerateOtp({ email }: IUserRegister) {
-  //   const user = await this.register({ email })
-  //   // await User.deleteAllToken(user?.id);
-  //   const newToken: any = {}; // await User.generateToken(user);
-
-  //   const { id, hashedToken: registerId } = user;
-  //   const { emailToken, id: token } = newToken;
-
-  //   await this.sendEmailOtpVerification(user, newToken);
-
-  //   return {
-  //     id,
-  //     email,
-  //     registerId,
-  //     token,
-  //     emailToken,
-  //   };
-  // }
-
-  public static async sendEmailOtpVerification({ email }: IUser, { emailToken: code }: any) {
-    return await sendVerificationCode({ email, code: code || "" });
-  }
-
-  public static async getUserByEmail(email: string) {
-    try {
-      const { hashedToken, password, ...rest } = await User.getByEmail(email);
-      return rest;
-    } catch (error) {
-      return null
-    }
-  }
-
-  public static getNewHashToken(): string {
-    return crypto.randomBytes(64).toString('hex')
-  }
-
-  public static async verifyUserOtp(verificationPayload: IUserVerification) {
-    if (!verificationPayload?.registerId) throw new Error(Errors.INVALID_TOKEN);
-    const user = await User.getByHashedToken(verificationPayload?.registerId);
-    if (!user) throw new Error(Errors.TOKEN_EXPIRED);
-    return null; // await User.getVerifyOtpUser(verificationPayload)
-  }
-
-
-  public async getAllUsers() {
-    const allUsers = await db.user.findMany({
-      select: {
-        email: true,
-        name: true,
-        id: true,
-        createdAt: true,
-        updatedAt: true,
-      }
-    });
-
-    return allUsers
   }
 
 }
